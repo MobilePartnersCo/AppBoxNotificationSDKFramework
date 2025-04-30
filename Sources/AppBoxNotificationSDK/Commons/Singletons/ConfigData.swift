f89a763180ea8e77548eb074680db179c5bdd01d
sangsoo <tkdtn2013@gmail.com>
2025-04-30 16:12:27 +0900

    test

--- Sources/AppBoxNotificationSDK/Commons/Singletons/ConfigData.swift
+++ Sources/AppBoxNotificationSDK/Commons/Singletons/ConfigData.swift
@@ -9,8 +9,5 @@ final class ConfigData {
     static let shared = ConfigData()
     private init() {}
     
-    var initalize: Bool = false
     var isFcmInit: Bool = false
-    var isDuplicateInit: Bool = false
-    var isDuplicateSave: Bool = false
 }
